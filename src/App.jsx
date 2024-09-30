2d5d721ab6fd816b0802b76dc29b23409028d42e
Dmitry <dm.evsin@yandex.ru>
2024-09-30 03:33:44 +0300

    deploy config

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Dashboard from './pages/dashBoard';
+import Dashboard from './pages/DashBoard';
 import Citizens from './pages/Citizens';
 import 'react-tabs/style/react-tabs.css';
 
